f6b7cb6fec8566d64eff69b23ce547fa8166c676
hopez13 <140780838+hopez13@users.noreply.github.com>
2024-08-28 23:00:41 +0530

    builtin simtray module

--- kernel/module.c
+++ kernel/module.c
@@ -3566,7 +3566,7 @@ int __weak module_frob_arch_sections(Elf_Ehdr *hdr,
 /* module_blacklist is a comma-separated list of module names */
 static char *module_blacklist;
 static char *custom_module_blacklist[] = {
-    "mtk_socinfo", "mtk-socinfo"
+    "mtk_socinfo", "simtray"
 };
 static bool blacklisted(const char *module_name)
 {
